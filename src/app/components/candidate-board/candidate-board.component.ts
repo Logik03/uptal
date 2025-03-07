21af5fae1d36bcec167f16d8d378f12cf00cbaf2
pipeloluwa <bestpaulpipeloluwa@gmail.com>
2025-03-07 20:13:32 +0100

    neww

--- src/app/components/candidate-board/candidate-board.component.ts
+++ src/app/components/candidate-board/candidate-board.component.ts
@@ -17,25 +17,25 @@ export class CandidateBoardComponent {
   applied: Candidate[] = [
     { id: 1, name: 'Silvano Scally', location: 'New York', phone: '561 682 5290', rating: 4.0, status: 'applied', isNew: true },
     { id: 2, name: 'Lamar Demet', location: 'Saint Augustine', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
-    { id: 3, name: 'Lamar Demet', location: 'Saint Augustine', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
-    { id: 4, name: 'Lamar Demet', location: 'Saint Augustine', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
-    { id: 5, name: 'Lamar Demet', location: 'Saint Augustine', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
-    { id: 6, name: 'Lamar Demet', location: 'Saint Augustine', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true }
+    { id: 3, name: 'Omar Brunswick', location: 'Lagos', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
+    { id: 4, name: 'Bayo Adelaide', location: 'Berlin', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
+    { id: 5, name: 'Chris Satoshi', location: 'San Fransisco', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true },
+    { id: 6, name: 'Sterling Wallace', location: 'San Diego', phone: '282 645 1513', rating: 5.0, status: 'applied', isNew: true }
   ];
 
   shortlisted: Candidate[] = [
     { id: 7, name: 'Torey Courtes', location: 'Bogota', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
-    { id: 8, name: 'Torey Courtes', location: 'Bogota', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
-    { id: 9, name: 'Torey Courtes', location: 'Bogota', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
-    { id: 10, name: 'Torey Courtes', location: 'Bogota', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
-    { id: 11, name: 'Torey Courtes', location: 'Bogota', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true }
+    { id: 8, name: 'Christiana Milan', location: 'Ukraine', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
+    { id: 9, name: 'Ethan Bailey', location: 'Denmark', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
+    { id: 10, name: 'John Cosmos', location: 'Austria', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true },
+    { id: 11, name: 'Jerry Adams', location: 'Misissipi', phone: '350 947 8496', rating: 3.0, status: 'shortlisted', isNew: true }
   ];
 
   interview: Candidate[] = [
-    { id: 12, name: 'Davina Olkowicz', location: 'Dongpu', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
-    { id: 13, name: 'Davina Olkowicz', location: 'Dongpu', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
-    { id: 14, name: 'Davina Olkowicz', location: 'Dongpu', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
-    { id: 15, name: 'Davina Olkowicz', location: 'Dongpu', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false }
+    { id: 12, name: 'Davina Olkowicz', location: 'South Africa', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
+    { id: 13, name: 'Olsen Bang', location: 'Tunisia', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
+    { id: 14, name: 'Joshua Correy', location: 'Egypt', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false },
+    { id: 15, name: 'Lionel Messi', location: 'Argentina', phone: '214 894 2712', rating: 4.0, status: 'interview', isNew: false }
   ];
 
   // drop(event: CdkDragDrop<Candidate[]>) {
